90ff8bfff80245e812ff9dec5e632faa26847df1
Roxanne Kelly <89264198+Roxannekelly@users.noreply.github.com>
2022-02-20 02:14:18 -0500

    updated files

--- script.js
+++ script.js
@@ -1,23 +1,40 @@
+
 function menu(){
-    var x= 
-    document.getElementById("myLinks");
-      if ( x.style.display === "block"){
-          x.style.display = "none";
-      } else {
-          x.style.display = "block";
-      }
+  var x= 
+  document.getElementById("myLinks");
+    if ( x.style.display === "block"){
+      x.style.display = "none";
+    } else {
+      x.style.display = "block";    
+  }
 }
 
 $(document).ready(function(){
+  $(".homeLink").click(function(){
+    $(this).show();
+  })
+})
+
+$(document).ready(function(){
+  $("#myLinks").click(function(){
+  $(this).hide();
+  })
+})
+
+
+
+$(document).ready(function(){
 $(window).scroll(function() {
-    $( ".main-menu" ).css( "background-color", "yellow" );
+  $( ".main-menu" ).css( "background-color", "yellow" );
   });
 });
 
 
+
+
+
 $(document).ready(function(){
-    $("input").focus(function(){
-      $(this).css("border", "1px solid yellow");
-    });
-    
+  $("input").focus(function(){
+    $(this).css("border", "1px solid yellow");
+    }); 
     });
